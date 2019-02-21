4c1efe074fcdc7b690007c2926629edbdf39cc1b
wangkaiwd <1162486176@qq.com>
2019-02-21 11:37:26 +0800

    feat: 整个项目的初始话，构建工具还有待考虑

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="javascript:;"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            <Button type="primary">
+              Learn React
+            </Button>
           </a>
         </header>
       </div>
