d2ba08cf858b8a357e17df621493fb37c8cb8b39
wangkaiwd <1162486176@qq.com>
2019-07-05 00:03:58 +0800

    feat: 继续优化代码

--- practice/demo03.ts
+++ practice/demo03.ts
@@ -1,21 +1,18 @@
-interface ClassMap {
-  [key: string]: string
-}
 const createButton = (content: string, className?: string): void => {
   const button: HTMLButtonElement = document.createElement('button');
   const div: HTMLElement = document.createElement('div');
   button.innerText = content;
+  button.classList.add('button');
   div.classList.add('col');
   if (className) {
     div.classList.add(className);
   }
   div.appendChild(button);
-  document.querySelector('.calculator-content').appendChild(div);
+  // 强制断言为HTMLElement
+  (<HTMLElement>document.querySelector('.calculator')).appendChild(div);
 };
 
-'Clear,÷,7,8,9,×,4,5,6,-,1,2,3,+,0,.,='.split(',')
-  .forEach((item: string): void => {
-    const classMap: ClassMap = { Clear: 'clear', 0: 'zero' };
-    const className: string | undefined = classMap[item] ? classMap[item] : undefined;
-    createButton(item, className);
-  });
+const texts: string[] = ['clear', '÷', '7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+texts.forEach((item: string): void => {
+  createButton(item, `item-${item}`);
+});
