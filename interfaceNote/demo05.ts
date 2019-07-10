db2fd1d6d4bf1296d899e8bcc44f6ff18b06aa74
wangkaiwd <1162486176@qq.com>
2019-07-10 23:42:17 +0800

    feat: 通过函数自执行返回了一个有属性的函数

--- interfaceNote/demo05.ts
+++ interfaceNote/demo05.ts
@@ -6,14 +6,25 @@ interface Add {
   opposite (a: number, b: number): number
 }
 
-// 这里感觉在校验 函数参数a,b的时候会有问题
-const add: Add = (a, b) => {
-  return a + b;
-};
-add.opposite = (a, b) => {
-  return a - b;
-};
+// // 这里感觉在校验 函数参数a,b的时候会有问题
+// const add: Add = (a, b) => {
+//   return a + b;
+// };
+// add.opposite = (a, b) => {
+//   return a - b;
+// };
+//
+// console.log('add', add(1, 2));
+// console.log('reduce', add.opposite(1, 2));
 
-console.log('add', add(1, 2));
-console.log('reduce', add.opposite(1, 2));
+const add: Add = (() => {
+  const x: any = (a: number, b: number) => {
+    return a + b;
+  };
+  x.opposite = (a: number, b: number) => {
+    return a - b;
+  };
+  return x;
+})();
+console.log('add', add.opposite(2, 1));
 export {};
