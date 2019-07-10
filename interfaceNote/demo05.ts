da4ad3d4367cdea53c8533e23c3d80d0e5336448
wangkaiwd <1162486176@qq.com>
2019-07-10 23:16:07 +0800

    feat: 用接口描述一个函数的属性仍然是函数的函数

--- interfaceNote/demo05.ts
+++ interfaceNote/demo05.ts
@@ -1,11 +1,19 @@
 interface Add {
+  // 描述当前函数：参数和返回值
   (a: number, b: number): number;
+
+  // 当前函数的属性opposite也是一个函数
+  opposite (a: number, b: number): number
 }
 
 // 这里感觉在校验 函数参数a,b的时候会有问题
 const add: Add = (a, b) => {
   return a + b;
 };
+add.opposite = (a, b) => {
+  return a - b;
+};
 
-console.log(add(1, 2));
+console.log('add', add(1, 2));
+console.log('reduce', add.opposite(1, 2));
 export {};
