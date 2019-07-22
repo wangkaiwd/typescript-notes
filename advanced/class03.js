d659db67a70a16a8220ff27c17ff33ec13ae611c
wangkaiwd <1162486176@qq.com>
2019-07-23 00:17:22 +0800

    feat: delete 旧的笔记代码

--- advanced/class03.js
+++
@@ -1,8 +0,0 @@
-// 接口继承类
-var Point = /** @class */ (function () {
-    function Point() {
-    }
-    return Point;
-}());
-var point3d = { x: 1, y: 2, z: 3 };
-console.log(point3d);
--- advanced/stringLiteral01.js
+++
@@ -1,6 +0,0 @@
-var handleEvent = function (ele, event) {
-    // doSomething
-};
-handleEvent(document.getElementById('hello'), 'click');
-// error argument of dbclick is not assignable to parameter of type 'EventNames'
-// handleEvent(document.getElementById('world'), 'dbclick');
